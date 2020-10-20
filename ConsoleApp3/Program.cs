dc457fd6cd5f8644a60476672e0454786e92b4f6
Jeendock <Jeendock@users.noreply.github.com>
2020-10-20 19:02:38 +0900

    c

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple apple = new Apple();
+            Banana banana = new Banana();
+
+            apple.Grow();
+            banana.Grow();
+        }
+
+        class Fruit
+        {
+            public virtual void Grow()
+            {                                                
+                Console.WriteLine("Grow");
+            }
+        }
+        class Apple : Fruit
+        {
+            public override void Grow()
+            {
+                Console.WriteLine("Apple");
+                base.Grow();
+            }
+        }
+        class Banana : Fruit
+        {
+            public override void Grow()
+            {
+                Console.WriteLine("Banana");
+                base.Grow();
+            }
+        }
+    }
+}
